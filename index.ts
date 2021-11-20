aadd5e5f5e51cd6101611ae2d0cf50a0c5be42ea
young-do <yd199323@gmail.com>
2021-11-20 15:03:48 +0900

    add ga

--- index.ts
+++ index.ts
@@ -48,6 +48,14 @@ const html = `
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./normalize.css" >
     <link rel="stylesheet" href="./index.css" >
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2LDBHRPKXL"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-2LDBHRPKXL');
+    </script>
     </head>
   <body>
     <div id="progress-bar"></div>
