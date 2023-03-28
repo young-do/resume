8cfa38ae66e2ea87e030f49a1052756086c1f625
young-do <yd199323@gmail.com>
2023-03-28 18:13:38 +0900

    github pages 배포 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://young-do.github.io',
+  base: '/resume',
+});
